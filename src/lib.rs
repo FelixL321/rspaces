a50ceb08b43cd7f074afa509e056e1e6d241f3b6
felix <s174296@student.dtu.dk>
2022-03-26 14:11:31 +0100

    initial space and tuple implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+mod sequential_space;
+mod space;
+
+pub use crate::sequential_space::SequentialSpace;
+pub use crate::space::Space;
+pub use crate::space::Tuple;
